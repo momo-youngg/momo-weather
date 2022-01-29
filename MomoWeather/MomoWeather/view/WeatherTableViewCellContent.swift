81db7185787da8918624dac7598151c987cb46c9
young-momo <yeongmoj94+dev@gmail.com>
2022-01-30 06:43:20 +0900

    modify: design of table view cell

--- MomoWeather/MomoWeather/view/WeatherTableViewCellContent.swift
+++ MomoWeather/MomoWeather/view/WeatherTableViewCellContent.swift
@@ -10,18 +10,30 @@ import UIKit
 class WeatherTableViewCellContent: UIView {
             
     @IBOutlet var cityName: UILabel!
-    @IBOutlet var weatherIcon: UILabel!
+    @IBOutlet var weatherIcon: UIImageView!
     @IBOutlet var temperature: UILabel!
     @IBOutlet var humidity: UILabel!
     @IBOutlet var backgroundView: UIView!
         
     func setWeatherData(weatherData: CurrentWeatherDataResponse) {
         cityName.text = weatherData.name
-        weatherIcon.text = weatherData.weather[0].icon
+        weatherIcon.image = UIImage(systemName: "heart")
+//        weatherData.weather[0].icon
         temperature.text = String(weatherData.main.temp)
         humidity.text = String(weatherData.main.humidity)
 
         GradientUtil.setGradientToView(gradientColor: GradientUtil.GradientColor.blue, view: backgroundView)
+        
+        setTemp(label: cityName)
+        setTemp(label: temperature)
+        setTemp(label: humidity)
+    }
+    
+    private func setTemp(label: UILabel) {
+        label.minimumScaleFactor = 0.1    //or whatever suits your need
+        label.adjustsFontSizeToFitWidth = true
+        label.lineBreakMode = .byClipping
+        label.numberOfLines = 0
     }
     
 }
