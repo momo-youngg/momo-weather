7528a9b417670d66e5e6bb57995e47f6a4d8c8c2
young-momo <yeongmoj94+dev@gmail.com>
2022-01-30 06:12:15 +0900

    modify: refactoring WeatherTableViewCell and WeatherTableViewCellContent
    delete auto layout code and move it to storyboard

--- MomoWeather/MomoWeather/view/WeatherTableViewCell.swift
+++ MomoWeather/MomoWeather/view/WeatherTableViewCell.swift
@@ -12,26 +12,17 @@ class WeatherTableViewCell: UITableViewCell {
     @IBOutlet var subview: WeatherTableViewCellContent!
             
     func setWeatherData(weatherData: CurrentWeatherDataResponse) {
-//        setBackgroundView()
         subview.setWeatherData(weatherData: weatherData)
     }
     
-    //TODO 매끄럽지 못한 배경색 및 코너 라운딩 이부분 너무 깔끔하지 못함
-//    func setBackgroundView() {
-//        GradientUtil.setGradientToView(gradientColor: GradientUtil.GradientColor.blue, view: self.contentView)
-//        self.layer.cornerRadius = 15;
-//        self.layer.masksToBounds = true;
-//    }
-
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
+        super.setSelected(selected, animated: animated)
     }
 
 }
--- MomoWeather/MomoWeather/view/WeatherTableViewCellContent.swift
+++ MomoWeather/MomoWeather/view/WeatherTableViewCellContent.swift
@@ -10,40 +10,18 @@ import UIKit
 class WeatherTableViewCellContent: UIView {
             
     @IBOutlet var cityName: UILabel!
-    @IBOutlet var weather: UILabel!
     @IBOutlet var weatherIcon: UILabel!
     @IBOutlet var temperature: UILabel!
     @IBOutlet var humidity: UILabel!
-    
-    var backgroundView: UIView?
+    @IBOutlet var backgroundView: UIView!
         
     func setWeatherData(weatherData: CurrentWeatherDataResponse) {
         cityName.text = weatherData.name
-        weather.text = weatherData.weather[0].main
         weatherIcon.text = weatherData.weather[0].icon
         temperature.text = String(weatherData.main.temp)
         humidity.text = String(weatherData.main.humidity)
-        
-        setBackgroundView()
-    }
-    
-    //TODO 매끄럽지 못한 배경색 및 코너 라운딩 이부분 너무 깔끔하지 못함
-    func setBackgroundView() {
-        if (backgroundView == nil) {
-            backgroundView = UIView()
-            addSubview(backgroundView!)
-            sendSubviewToBack(backgroundView!)
-            backgroundView!.translatesAutoresizingMaskIntoConstraints = false
-            backgroundView!.topAnchor.constraint(equalTo: topAnchor).isActive = true
-            backgroundView!.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
-            backgroundView!.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
-            backgroundView!.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        }
 
-        GradientUtil.setGradientToView(gradientColor: GradientUtil.GradientColor.blue, view: self)
-        self.layer.cornerRadius = 15;
-        self.layer.masksToBounds = true;
+        GradientUtil.setGradientToView(gradientColor: GradientUtil.GradientColor.blue, view: backgroundView)
     }
-
     
 }
