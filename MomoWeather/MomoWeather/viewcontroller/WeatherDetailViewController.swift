afb40f0f0d325f6619790ff792244e59991ad27b
young-momo <yeongmoj94+dev@gmail.com>
2022-01-30 16:35:48 +0900

    add: WeatherDetailViewController and storyboard

---
+++ MomoWeather/MomoWeather/viewcontroller/WeatherDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherDetailViewController.swift
+//  MomoWeather
+//
+//  Created by momo on 2022/01/30.
+//
+
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+    
+    var weather: CurrentWeatherDataResponse!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(weather!)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
