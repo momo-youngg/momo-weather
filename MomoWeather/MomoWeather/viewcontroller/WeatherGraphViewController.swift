92bf4d0a06edd7529c0c5cc653f34871fb7ed355
young-momo <yeongmoj94+dev@gmail.com>
2022-02-02 02:05:25 +0900

    fix: gradient not filled when orientation is changed at graph view

--- MomoWeather/MomoWeather/viewcontroller/WeatherGraphViewController.swift
+++ MomoWeather/MomoWeather/viewcontroller/WeatherGraphViewController.swift
@@ -17,12 +17,18 @@ class WeatherGraphViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         titleLabel.setTextHeightToMaxFit()
+        fetchWeatherForecastData()
         GradientUtil.setGradientToView(gradientColor: weatherData.weather[0].gradientColor, view: backgroundView)
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.detectOrientation), name: NSNotification.Name("UIDeviceOrientationDidChangeNotification"), object: nil)
+    }
+    
+    private func fetchWeatherForecastData() {
         openWhetherClient.get5Day3HourForecastData(lat: weatherData.coord.lat,
                                                    lon: weatherData.coord.lon) { result, forecast in
+            guard result else {
+                return
+            }
             let graphInfo: GraphInfo = GraphInfo(graphKeyInfo: forecast.graphKeyInfo,
                                                  graphKeyVerticalLinePredicate: { $0.contains(" ") },
                                                  leftGraphValueInfo: [GraphInfo.GraphValueInfo(description: "최저기온",
@@ -45,6 +51,12 @@ class WeatherGraphViewController: UIViewController {
                 self.graphView.graphInfo = graphInfo
             }
         }
+
     }
+    
+    @objc func detectOrientation() {
+        GradientUtil.setGradientToView(gradientColor: weatherData.weather[0].gradientColor, view: backgroundView)
+    }
+
     
 }
